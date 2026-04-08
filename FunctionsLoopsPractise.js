5ba85f553f62bc8adf5d22e2c572c2e5bb28886c
Huma-Hassan014 <huma.hassan453449@gmail.com>
2026-04-09 01:48:13 +0500

    task 4 5 6

--- FunctionsLoopsPractise.js
+++ FunctionsLoopsPractise.js
@@ -36,3 +36,44 @@ let f = prompt("Enter the Number to find factorial: ")
 f = Number(f)
 console.log(factorial(f))
 
+//Level 2
+//Task 4
+const reverse = (num) => {
+    let rev = 0;
+
+    while( num > 0){
+        let digit = num % 10; //extract the last value
+        rev  = rev * 10 + digit;
+         num = Math.floor( num / 10) //remove the last digit
+    }
+     return rev
+}
+console.log(reverse(1234))
+
+//Task 5
+const counting = (m) => {
+  let count = 0;
+  while (m > 0) {
+    m = Math.floor(m / 10);
+    count++;
+  }
+  return count;
+};
+console.log(counting(1234));
+
+//Task 6
+
+const Palindrom = (num) => {
+    let rev = 0;
+    let orig = num
+    while( num > 0){
+        let digit = num % 10; 
+        rev  = rev * 10 + digit;
+         num = Math.floor( num / 10) 
+    }
+     return orig === rev
+}
+let h = prompt("Enter the Number to find the Palindron: ")
+h = Number(h)
+console.log(Palindrom(h))
+
